bf36bf6a4a13ac31e3bf786220308a702f542dca
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-07 16:12:10 -0500

    Create methods for updating grid and cells

--- game.py
+++ game.py
@@ -27,3 +27,37 @@ class GameOfLife:
                     pygame.draw.rect(self.surface, self.active_color, [row * self.scale, col * self.scale, self.scale - self.offset, self.scale - self.offset])
                 else:
                     pygame.draw.rect(self.surface, self.inactive_color, [row * self.scale, col * self.scale, self.scale - self.offset, self.scale - self.offset])
+
+    def update_grid(self):
+        updated_grid = self.grid.copy()
+        for row in range(updated_grid.shape[0]):
+            for col in range(updated_grid.shape[1]):
+                updated_grid[row, col] = self.update_cell(row, col)
+
+        self.grid = updated_grid
+
+    def update_cell(self, x, y):
+        current_state = self.grid[x, y]
+        alive_neighbors = 0
+
+        # Check surrounding cells
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                try:
+                    if i == 0 and j == 0:
+                        continue
+                    elif self.grid[x + i, y + j]:
+                        alive_neighbors += 1
+                except:
+                    continue
+
+        if current_state and alive_neighbors < 2:
+            return False
+        elif current_state and (alive_neighbors == 2 or alive_neighbors == 3):
+            return True
+        elif current_state and alive_neighbors > 3:
+            return False
+        elif ~current_state and alive_neighbors == 3:
+            return True
+        else:
+            return current_state
