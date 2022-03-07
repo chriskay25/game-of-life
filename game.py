9fd59ff1757f3291dc41c7575eed5332dff1b553
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-07 15:18:57 -0500

    Make a game loop

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 
 pygame.init()
@@ -5,3 +6,13 @@ pygame.init()
 WIDTH, HEIGHT = 1920, 1080
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((255,255,255))
+    pygame.display.update()
